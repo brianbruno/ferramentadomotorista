e5fe13c900d983d5a704ac568dfa16c209ef1d47
Brian Bruno <brian.bruno@outlook.com>
2017-11-02 20:41:52 -0200

    CRUD

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -4,10 +4,10 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Laravel 5.5 CRUD Example from scratch</h2>
+            <h2>Clientes Ferramenta do Motorista</h2>
         </div>
         <div class="pull-right">
-            <a class="btn btn-success" href="{{ route('clientes.create') }}"> Create New Client</a>
+            <a class="btn btn-success" href="{{ route('clientes.create') }}"> Cadastrar novo cliente</a>
         </div>
     </div>
 </div>
@@ -20,19 +20,20 @@
 
 <table class="table table-bordered">
     <tr>
-        <th>No</th>
+        <th>ID</th>
         <th>Nome</th>
         <th>Sobrenome</th>
+        <th>Email</th>
         <th width="280px">Action</th>
     </tr>
     @foreach ($clientes as $cliente)
     <tr>
-        <td>{{ ++$i }}</td>
+        <td>{{ $cliente->id }}</td>
         <td>{{ $cliente->nome}}</td>
         <td>{{ $cliente->sobrenome}}</td>
         <td>
             <a class="btn btn-info" href="{{ route('clientes.show',$cliente->id) }}">Show</a>
-            <a class="btn btn-primary" href="{{ route('clientes.edit',$cliente->id) }}">Edit</a>
+            <a class="btn btn-primary" href="{{ route('clientes.edit',$cliente->id) }}">Editar</a>
             {!! Form::open(['method' => 'DELETE','route' => ['clientes.destroy', $cliente->id],'style'=>'display:inline']) !!}
             {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
             {!! Form::close() !!}
